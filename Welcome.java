3b668392618755464bda1e24ed72dbdb2885b2d2
olusegun olusola <sholaolusegun@gmail.com>
2021-02-19 21:45:36 +0100

    third commit

--- Welcome.java
+++ Welcome.java
@@ -6,7 +6,7 @@ public static void main(String args[]){
 int number1;
 Scanner input = new Scanner(System.in);
 number1 = input.nextInt();
-System.out.println("welcome to java, java is an interstring programming language");
+System.out.println("welcome to java, java is an interstring programming languages");
 //System.out.println("first argument is " + args[0]);
 
 }
