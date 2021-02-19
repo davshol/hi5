5cf4b0ef2404ed4a20224bf7f8f87eec3db2176b
olusegun olusola <sholaolusegun@gmail.com>
2021-02-19 08:57:16 +0100

    init commit

---
+++ Welcome.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Welcome{
+
+public static void main(String args[]){
+int number1;
+Scanner input = new Scanner(System.in);
+number1 = input.nextInt();
+System.out.println("welcome to java");
+//System.out.println("first argument is " + args[0]);
+
+}
+
+
+}
